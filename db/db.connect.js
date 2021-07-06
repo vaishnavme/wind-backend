04d329e0ff00262ad8a6eca4f844cc1a47f395d7
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-06 16:22:50 +0530

    database connection bug fix

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,6 +4,7 @@ function dbConnection() {
     mongoose.connect(process.env.MONGODB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
+        useCreateIndex: true,
     })
     .then(() => console.log("Connected Successfully..."))
     .catch(error => console.error("mongoose connection failed", error))
