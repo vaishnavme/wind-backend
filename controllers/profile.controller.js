087d6428acea6c8f630616dfd6b28c6c62c35527
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-12 11:34:01 +0530

    feat: get user profile

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,6 +1,29 @@
 const { User } = require("../models/user.model");
 const { extend } = require("lodash");
 
+const getUserProfile = async(req, res) => {
+    const { profileId } = req.params;
+    try {
+        const profile = await User.findById(profileId);
+        if(!profile) return res.status(404).json({
+            success: false,
+            message: "User not found"
+        })
+
+        res.json({
+            success: true,
+            profile,
+            message: "User found"
+        })
+    } catch(err) {
+        console.log(err);
+        res.json({
+            success: false,
+            message: `Error Occured: ${err}`
+        })
+    }
+}
+
 const updateProfile = async(req, res) => {
     const { user } = req;
     const { profileUpdates } = req.body;
@@ -31,5 +54,6 @@ const updateProfile = async(req, res) => {
 }
 
 module.exports = {
+    getUserProfile,
     updateProfile
 }
