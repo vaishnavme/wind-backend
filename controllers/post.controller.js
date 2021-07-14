067adcfa959584df46eebc284731c1928df2a37b
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-14 22:11:04 +0530

    feat: unused code removed

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -35,21 +35,12 @@ const createNewPost = async(req, res) => {
     const { user } = req;
     const { post } = req.body;
     try {
-        const userAccount = await User.findById(user.userId);
-
         const newPost = new Post({
             creator: user.userId,
             content: post
         })
         let savedPost = await newPost.save()
-        savedPost = await savedPost.populate(populateCreator).execPopulate()
-        // update user post collection
-        if(!userAccount) return res.status(404).json({
-            success: false,
-            message: "User not found"
-        })
-        userAccount.posts.push(savedPost._id)
-        await userAccount.save();
+      
         res.json({
             success: true,
             savedPost,
@@ -64,69 +55,13 @@ const createNewPost = async(req, res) => {
     }
 }
 
-const updateUserPost = async(req, res) => {
-    const { user } = req;
-    const { postId } = req.params;
-    const { postUpdate } = req.body;
-    try {
-        const post = await Post.findById(postId);
-        const userAccount = await User.findById(user.userId);
-        // check for post and user 
-        if(!post) return res.status(404).json({
-            success: false,
-            message: "Post not found"
-        })
-        if(!userAccount) return res.status(404).json({
-            success: false,
-            message: "User not found"
-        })
-    
-        const updated = extend(post, {
-            post, content: postUpdate
-        })
-        const updatedPost = await updated.save();
-        res.json({
-            success: true,
-            updatedPost,
-            message: "Post successfully updated!"
-        })
-
-    } catch(err) {
-        console.log(err);
-        res.json({
-            success: false,
-            message: `Error Ocuured: ${err}`
-        })
-    }
-}
-
 const deleteUserPost = async(req, res) => {
-    const { user } = req;
     const { postId } = req.params;
     try {
-        const post = await Post.findById(postId);
-        const userAccount = await User.findById(user.userId);
-
-        if(!post) return res.status(404).json({
-            success: false,
-            message: "Post not found"
-        })
-        if(!userAccount) return res.status(404).json({
-            success: false,
-            message: "User not found"
-        })
-
-        const userId = user.userId.toString();
-        const creatorId = post.creator.toString();
-        if(userId === creatorId) {
-            const deletedPost = await Post.findByIdAndDelete(postId);
-            userAccount.posts.splice(userAccount.posts.indexOf(deletedPost), 1);
-            userAccount.bookmarks.splice(userAccount.bookmarks.indexOf(deletedPost), 1);
-            await userAccount.save();
-        }
+        const deletedPost = await Post.findByIdAndDelete(postId);
         res.json({
             success: true,
-            deletedId: postId,
+            deletedId: deletedPost,
             message: "Post successfully deleted!"
         })
 
@@ -143,6 +78,5 @@ const deleteUserPost = async(req, res) => {
 module.exports = {
     getPostById,
     createNewPost,
-    updateUserPost,
     deleteUserPost,
 }
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken");
-const { getPostById, createNewPost, deleteUserPost, updateUserPost } = require("../controllers/post.controller");
+const { getPostById, createNewPost, deleteUserPost } = require("../controllers/post.controller");
 const feed = require("../controllers/feed.controller");
 
 router.use(verifyToken);
 // post operations
 router.get("/single/:postId", getPostById);
 router.post("/", createNewPost);
-router.post("/update/:postId", updateUserPost);
 router.delete("/:postId", deleteUserPost);
 
 // feed
