4197245bb84ddf0a811fd8ae7080d3a1ff72752c
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-01 23:16:20 +0530

    Code refactor

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -1,27 +1,31 @@
 const { Notification } = require("../models/notification.model");
 
 const populateData = {
-    path: "sourceUser",
-    select: "name username profilePhoto"
-}
+  path: "sourceUser",
+  select: "name username profilePhoto",
+};
 
-const getUserNotification = async(req, res) => {
-    const { user } = req;
-    try {
-        const notification = await Notification.find({targetUser: user.userId}).populate(populateData);
+const getUserNotification = async (req, res) => {
+  const { user } = req;
+  try {
+    const notification = await Notification.find({
+      targetUser: user.userId,
+    }).populate(populateData);
 
-        const allNotifications = notification.sort((notify1, notify2) => notify2.time - notify1.time)
-        res.json({
-            success: true,
-            allNotifications
-        })
-    } catch(err) {
-        console.log(err);
-        res.json({
-            success: false,
-            message: `Error Occured: ${err}`
-        })
-    }
-}
+    const allNotifications = notification.sort(
+      (notify1, notify2) => notify2.time - notify1.time
+    );
+    res.json({
+      success: true,
+      allNotifications,
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      success: false,
+      message: `Error Occured: ${err}`,
+    });
+  }
+};
 
 module.exports = getUserNotification;
--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,70 +1,88 @@
 const { User } = require("../models/user.model");
 const { extend } = require("lodash");
 
-const getUserProfile = async(req, res) => {
-    const { profileId } = req.params;
-    try {
-        
-        let profile = await User.findById(profileId)
-                            .populate({
-                                path: "posts", 
-                                populate: {path: "creator", select: "name username profilePhoto"}
-                            })
-                            .populate({path: "following", select: "name username profilePhoto"})
-                            .populate({path: "followers", select: "name username profilePhoto"})
+const getUserProfile = async (req, res) => {
+  const { profileId } = req.params;
+  try {
+    let profile = await User.findById(profileId)
+      .populate({
+        path: "posts",
+        populate: { path: "creator", select: "name username profilePhoto" },
+      })
+      .populate({ path: "following", select: "name username profilePhoto" })
+      .populate({ path: "followers", select: "name username profilePhoto" });
 
-        let sortProfilePost = profile.posts.sort((post1, post2) => post2.createdAt - post1.createdAt)
-        profile.posts = sortProfilePost;
-        
-        if(!profile) return res.status(404).json({
-            success: false,
-            message: "User not found"
-        })
+    // getPopulate Posts
+    const userPosts = profile.posts;
+    // getPopulate Data
+    const userProfile = {
+      _id: profile._id,
+      name: profile.name,
+      username: profile.username,
+      email: profile.email,
+      bio: profile.bio,
+      profilePhoto: profile.profilePhoto,
+      following: profile.following,
+      followers: profile.followers,
+    };
 
-        res.json({
-            success: true,
-            profile,
-            message: "User found"
-        })
-    } catch(err) {
-        console.log(err);
-        res.json({
-            success: false,
-            message: `Error Occured: ${err}`
-        })
-    }
-}
+    let sortProfilePost = userPosts.sort(
+      (post1, post2) => post2.createdAt - post1.createdAt
+    );
 
-const updateProfile = async(req, res) => {
-    const { user } = req;
-    const { profileUpdates } = req.body;
-    try {
-        const userAccount = await User.findById(user.userId);
-        if(!userAccount) return res.status(404).json({
-            success: false,
-            messsage: "No user found"
-        })
+    if (!profile)
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
 
-        const updatedUserProfile = extend(userAccount, {
-            userAccount, ...profileUpdates
-        })
+    res.json({
+      success: true,
+      profile: userProfile,
+      posts: userPosts,
+      message: "User found",
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      success: false,
+      message: `Error Occured: ${err}`,
+    });
+  }
+};
 
-        await updatedUserProfile.save();
+const updateProfile = async (req, res) => {
+  const { user } = req;
+  const { profileUpdates } = req.body;
+  try {
+    const userAccount = await User.findById(user.userId);
+    if (!userAccount)
+      return res.status(404).json({
+        success: false,
+        messsage: "No user found",
+      });
 
-        res.json({
-            success: true,
-            updatedUserProfile
-        })
-    } catch(err) {
-        console.log(err)
-        res.json({
-            success: false,
-            message: `Error Occured: ${err}`
-        })
-    }
-}
+    const updatedUserProfile = extend(userAccount, {
+      userAccount,
+      ...profileUpdates,
+    });
+
+    await updatedUserProfile.save();
+
+    res.json({
+      success: true,
+      updatedUserProfile,
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      success: false,
+      message: `Error Occured: ${err}`,
+    });
+  }
+};
 
 module.exports = {
-    getUserProfile,
-    updateProfile
-}
+  getUserProfile,
+  updateProfile,
+};
